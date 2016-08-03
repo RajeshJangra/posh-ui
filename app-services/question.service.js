a385dd045cecac86036470e5df10c525e48c6003
gauravagrawal <gauravagrawal@XE-T-GAURAVA.XEBIAINDIA.LOCAL>
2016-08-03 12:56:23 +0530

    refactored

--- app-services/question.service.js
+++ app-services/question.service.js
@@ -1,37 +1,37 @@
 (function () {
-    'use strict';
+	'use strict';
 
-    angular
-        .module('app')
-        .factory('QuestionsService', Service);
+	angular
+	.module('app')
+	.factory('QuestionsService', Service);
 
-    function Service($location, $http, $localStorage) {
-        var service = {};
+	function Service($location, $http, $localStorage) {
+		var service = {};
 
-        service.getQuestions = getQuestions;
+		service.getQuestions = getQuestions;
 		service.submitQuiz = submitQuiz;
 
-        return service;
+		return service;
 
-        function getQuestions(data, callback) {
-            $http.get('http://localhost:8080/api/course/'+data.course.id+'/start/'+data.empId)
-                  .success(function (response) {
-                        callback(response);
-                })
-				.error(function(response){
-					$location.path('/login');
-				});
-        }
-		
-			function submitQuiz(attemptId, questions, callback) {
-            $http.post('http://localhost:8080/api/course/submit/'+attemptId, questions)
-                  .success(function (response) {
-                        callback(response);
-                })
-				.error(function(response){
-					$location.path('/login');
-				});
-        }
-        
-    }
+		function getQuestions(data, callback) {
+			$http.get('http://localhost:8080/api/course/'+data.course.id+'/start/'+data.empId)
+			.success(function (response) {
+				callback(response);
+			})
+			.error(function(response){
+				$location.path('/login');
+			});
+		}
+
+		function submitQuiz(attemptId, questions, callback) {
+			$http.post('http://localhost:8080/api/course/submit/'+attemptId, questions)
+			.success(function (response) {
+				callback(response);
+			})
+			.error(function(response){
+				$location.path('/login');
+			});
+		}
+
+	}
 })();
