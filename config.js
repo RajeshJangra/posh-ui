5ef62e7cbccda352dc707ce2b617b23a16cfe507
jitu281287 <jitender.singh281287@gmail.com>
2016-08-26 11:18:15 +0530

    UI changes

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 angular.module('induction.config', [
 ]).constant('constants',
 		{
-	baseUrl: "http://localhost:8080/induction/",
+	baseUrl: "http://192.168.100.20:8080/induction/",
 	url:{
 		auth:{
 			url: 'api/auth',
